ee7826c5805507dcff2ae4cd78cc418218509f3f
Alejandro Garcia Marchena <alejandro.garciamarchena.external@atos.net>
2017-10-04 18:07:19 +0200

    list of articles in app component

--- angular-reddit/src/app/app.component.ts
+++ angular-reddit/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model'; // <-- import this
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  articles:Article[];
+
+  constructor(){
+    this.articles=[
+      new Article("primer articulo", "primer link"),
+      new Article("segundo articulo", "segundo link"),
+      new Article("tercer articulo", "tercer link"),
+    ];
+  }
 
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
